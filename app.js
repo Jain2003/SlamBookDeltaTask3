a62ed99e587218c4a51f4d3fe2161a93ea1a4cf2
Sahaj Jain <95215600+Jain2003@users.noreply.github.com>
2022-08-09 17:28:07 +0530

    added better authentication using passport package

--- app.js
+++ app.js
@@ -2,24 +2,40 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const md5 = require("md5");
 
 const app = express();
 
-app.use(express.static(__dirname+"/public/"));
+app.use(express.static(__dirname + "/public/"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect("mongodb://localhost:27017/userDB");
+app.use(session({
+  secret: "idk what to type",
+  resave: false,
+  saveUninitialized: false
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+// mongoose.set("userCreateIndex",true);
+
+// mongoose.connect("mongodb+srv://admin-Sahaj:Sahaj@cluster0.cnxn7mw.mongodb.net/userDB");
 
 // schemas
-const userSchema = {
+const userSchema = new mongoose.Schema({
   username: String,
   department: String,
   password: String
-}
+});
+userSchema.plugin(passportLocalMongoose);
 
 const activeUserSchema = {
   username: String,
@@ -40,6 +56,11 @@ const commentSchema = {
 // documents
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 const ActiveUser = new mongoose.model("ActiveUser", activeUserSchema);
 
 const SearchedUser = new mongoose.model("SearchedUser", searchUserSchema);
@@ -50,14 +71,15 @@ var activeUsername = "";
 
 var searchedUsername = "";
 
-function myFunc(){
+function myFunc() {
   console.log("no delete till now.");
 }
-var success = true;
+// var success = true;
 
 app.get("/", function(req, res) {
   res.render("home");
 });
+
 
 app.get("/login", function(req, res) {
   res.render("login", {
@@ -72,23 +94,35 @@ app.get("/register", function(req, res) {
 });
 
 app.get("/dashboard", function(req, res) {
-  res.render("dashboard", {
-    activeUser: activeUsername,
-    warning: "Comment Here"
-  });
+  if (req.isAuthenticated()) {
+    res.render("dashboard", {
+      activeUser: activeUsername,
+      warning: "Comment Here"
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.get("/comment", function(req, res) {
-  res.render("comment", {
-    comments: [],
-    func:myFunc(),
-    warning: "enter a username in search box."
-  })
-})
+  if (req.isAuthenticated()) {
+    res.render("comment", {
+      comments: [],
+      func: myFunc(),
+      warning: "Enter a username in search box."
+    });
+  } else {
+    res.redirect("/");
+  }
+});
 
 
 app.post("/", function(req, res) {
   res.render("home");
+})
+
+
+app.get("/logout",function(req,res){
   ActiveUser.deleteMany(function(err) {
     if (err) {
       console.log(err);
@@ -102,47 +136,83 @@ app.post("/", function(req, res) {
     } else {
       console.log("All searched Items cleared.");
     }
-  })
+  });
+  req.logout(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect("/");
+    }
+  });
 })
 
 app.post("/login", function(req, res) {
   if (req.body.username === undefined) {
     res.render("login", {
-      warning: "enter your details"
-    })
-  } else {
-    User.findOne({
-      username: req.body.username
-    }, function(err, foundUser) {
-      if (err) {
-        console.log(err);
-      } else {
-        if (foundUser) {
-          if (foundUser.password === req.body.password) {
-            var activeLogin = new ActiveUser({
-              username: req.body.username,
-              department: foundUser.department
-            });
-            activeUsername = req.body.username;
-            activeLogin.save(async function(err) {
-              if (err) {
-                console.log(err);
-              } else {
-                await res.redirect("/dashboard")
-              }
-            })
-          } else {
-            res.render("login", {
-              warning: "Wrong Password"
-            });
-          }
-        } else {
-          res.redirect("/register");
-        }
-      }
+      warning: "Enter your details"
     })
   }
-});
+  else {
+    const userDetails = new User ({
+      username: req.body.username,
+      password: req.body.password
+    });
+
+    req.login(userDetails, function(err){
+      if(err){
+        console.log(err);
+      } else {
+        var activeLogin = new ActiveUser({
+          username: req.body.username,
+          department: req.body.department
+        })
+        activeLogin.save(function(err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("active user ");
+          }
+        })
+        passport.authenticate("local")(req,res, function(){
+          activeUsername = req.body.username;
+          res.redirect("/dashboard")
+            // res.render("dashboard", {
+            //   activeUser: activeUsername,
+            //   warning: "Comment Below"
+            // })
+          })
+        }
+      })
+    }
+
+
+    // User.findOne({
+    //   username: req.body.username
+    // }, function(err, foundUser) {
+    //   if (err) {
+    //     console.log(err);
+    //   } else {
+    //     if (foundUser) {
+    //       console.log(foundUser);
+    //       if (foundUser.password === md5(req.body.password)) {
+    //         var activeLogin = new ActiveUser({
+    //           username: req.body.username,
+    //           department: foundUser.department
+    //         });
+    //
+    //         })
+    //       } else {
+    //         res.render("login", {
+    //           warning: "Wrong Password"
+    //         });
+    //       }
+    //     } else {
+    //       res.redirect("/register");
+    //     }
+    //   }
+    })
+  // }
+// });
 
 app.post("/register", function(req, res) {
   if (req.body.username === undefined) {
@@ -164,33 +234,27 @@ app.post("/register", function(req, res) {
         if (foundUser) {
           console.log(foundUser);
           res.render("register", {
-            warning: "username already in use try another."
+            warning: "Username already in use try another."
           });
         } else {
-          newUser.save(function(err) {
+          User.register({
+            username: req.body.username,
+            department: req.body.department
+          }, req.body.password, function(err, user) {
             if (err) {
               console.log(err);
             } else {
-              if (req.body.username != undefined) {
-                activeUsername = req.body.username;
-                var activeLogin = new ActiveUser({
-                  username: req.body.username,
-                  department: req.body.department
+              passport.authenticate("local")(req, res, function() {
+                console.log("User saved");
+                res.render("login", {
+                  warning: "The credentials are saved! LOGIN!"
                 });
-                activeLogin.save(async function(err) {
-                  if (err) {
-                    console.log(err);
-                  } else {
-                    console.log("active User saved");
-                    res.redirect("/dashboard");
-                  }
-                })
-              }
+              })
             }
-          })
+          });
         }
       }
-    })
+    });
   }
 });
 
@@ -217,7 +281,7 @@ app.post("/dashboard", function(req, res) {
             } else {
               res.render("dashboard", {
                 activeUser: activeUsername,
-                warning: "CommentSaved, click on the comments button to see all the comments, Or comment on ur other friends."
+                warning: "Comment Saved, click on the comments button to see all the comments, Or comment on ur other friends."
               })
             }
           })
@@ -233,7 +297,7 @@ app.post("/comment", function(req, res) {
     res.render("comment", {
       comments: [],
       func: myFunc(),
-      warning:"Enter a username in search bar."
+      warning: "Enter a username in search bar."
     })
   } else {
     Comment.find({
@@ -245,13 +309,13 @@ app.post("/comment", function(req, res) {
       } else {
         console.log(foundRecordArray);
         if (foundRecordArray.length != 0) {
-          function deleteFunc(){
+          function deleteFunc() {
             Comment.deleteOne({
-              commentBy:foundRecordArray[0].commentBy
-            },function(err){
-              if(err){
+              commentBy: foundRecordArray[0].commentBy
+            }, function(err) {
+              if (err) {
                 console.log(err);
-              }else{
+              } else {
                 console.log(foundRecordArray[0].commentBy);
                 console.log("comment deleted");
               }
@@ -262,12 +326,12 @@ app.post("/comment", function(req, res) {
             func: deleteFunc(),
             warning: "Found Records"
           })
-        }else{
+        } else {
           console.log("no comments found");
-          res.render("comment",{
-            comments:foundRecordArray,
-            func:myFunc,
-            warning:"no  comments found"
+          res.render("comment", {
+            comments: foundRecordArray,
+            func: myFunc,
+            warning: "no  comments found"
           })
         }
       }
